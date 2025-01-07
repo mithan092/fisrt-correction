4323f30702c2ef48808e5bbc93fd479bc9a9e204
mithan092 <mithanmusreya@gmail.com>
2025-01-07 19:20:08 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+
+export default function Home() {
+  return (
+          <>
+            <h1 className="text-2xl bg-green-300 font-bold uppercase mg-12 text-center text-dark dark-text-light"> Hello sir...... my first project in next js
+                <br /> because i have not a laptop but now is new parchased a good laptop sir 
+                       so give one chance  
+                       i promise  SUBMMITED any assignment and project 
+                     <br></br>
+            </h1>
+                 <h4 className="bg-blue-700 text-white text-center ">
+                     Regards: Mithan kumar 03003448661 <br />
+                     Roll no: 00455092
+                 </h4>
+           </>
+
+);
+}
+
+
